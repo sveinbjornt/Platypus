a9f184727352b55b34e3dd9d7e4c35fe5792a19d
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-01-06 22:19:34 +0000

    Updating copyright to 2012

--- ScriptExec/ScriptExecController.m
+++ ScriptExec/ScriptExecController.m
@@ -1,6 +1,6 @@
 /*
     ScriptExec - binary bundled into Platypus-created applications
-    Copyright (C) 2003-2011 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
+    Copyright (C)  Sveinbjorn Thordarson <sveinbjornt@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
