5330f4ba43eba11568292b4e25ad8f25aa6c1e4e
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-01-06 22:19:34 +0000

    Updating copyright to 2012

--- PlatypusApplication/ProfilesController.m
+++ PlatypusApplication/ProfilesController.m
@@ -1,6 +1,6 @@
 /*
     Platypus - program for creating Mac OS X application wrappers around scripts
-	Copyright (C) 2003-2011 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
+	Copyright (C)  Sveinbjorn Thordarson <sveinbjornt@gmail.com>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
