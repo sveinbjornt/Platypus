0845cf601323087d7732c43105b5d5fd244017b2
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-02-26 23:28:12 +0000

    Fixing uninstall script

--- Scripts/UninstallPlatypus.sh
+++ Scripts/UninstallPlatypus.sh
@@ -4,20 +4,20 @@ cd "$1"
 
 sh UninstallCommandLineTool.sh
 
-if [ -e "~/Library/Application Support/Platypus" ]
+if [ -e ~/Library/Application\ Support/Platypus ]
 then
     echo "Deleting application support folder..." > /dev/stderr
-    mv "~/Library/Application Support/Platypus" ~/.Trash/PlatypusApplicationSupport-TRASHED
+    mv ~/Library/Application\ Support/Platypus ~/.Trash/PlatypusApplicationSupport-TRASHED
 fi
 
-if [ -e "~/Library/Preferences/org.sveinbjorn.platypus.plist" ]
+if [ -e ~/Library/Preferences/org.sveinbjorn.platypus.plist ]
 then
     echo "Deleting Platypus preferences..." > /dev/stderr
-    mv "~/Library/Preferences/org.sveinbjorn.platypus.plist" ~/.Trash/org.sveinbjorn.platypus-TRASHED.plist
+    mv ~/Library/Preferences/org.sveinbjorn.platypus.plist ~/.Trash/org.sveinbjorn.platypus-TRASHED.plist
 fi
 
-if [ -e "$1/../../../Platypus.app" ]
+if [ -e $1/../../../Platypus.app ]
 then
     echo "Moving Platypus.app to Trash" > /dev/stderr
-    mv "$1/../../../Platypus.app" ~/.Trash/Platypus-TRASHED.app
+    mv $1/../../../Platypus.app ~/.Trash/Platypus-TRASHED.app
 fi
