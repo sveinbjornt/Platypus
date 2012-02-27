e6c6ceb862827cfe51d25b038a50d277f1afe1b3
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-02-27 16:27:24 +0000

    Migrated man page to HTML instead of PDF, to make for slimmer download

---
+++ CommandLineTool/update_website_docs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp platypus.man.pdf root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/platypus.man.pdf
+scp platypus.man.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/platypus.man.html
