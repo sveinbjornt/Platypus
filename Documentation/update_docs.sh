e4fadd25ec9e4c2c47f98fdef6005d96f08e6bf2
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2012-03-12 15:01:27 +0000

    Adding doc update script

---
+++ Documentation/update_docs.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp PlatypusDocumentation.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/PlatypusDocumentation.html
+scp -r images root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/
