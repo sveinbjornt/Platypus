458834f2d97892c91059d079e01d108652d5e6b5
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2013-07-07 19:52:12 +0000

    Add $RANDOM to filename in Trash

--- Scripts/UninstallPlatypus.sh
+++ Scripts/UninstallPlatypus.sh
@@ -13,17 +13,17 @@ sh UninstallCommandLineTool.sh
 if [ -e ~/Library/Application\ Support/Platypus ]
 then
     echo "Deleting application support folder..." > /dev/stderr
-    mv ~/Library/Application\ Support/Platypus ~/.Trash/PlatypusApplicationSupport-TRASHED
+    mv ~/Library/Application\ Support/Platypus ~/.Trash/PlatypusApplicationSupport-TRASHED-$RANDOM
 fi
 
 if [ -e ~/Library/Preferences/org.sveinbjorn.platypus.plist ]
 then
     echo "Deleting Platypus preferences..." > /dev/stderr
-    mv ~/Library/Preferences/org.sveinbjorn.platypus.plist ~/.Trash/org.sveinbjorn.platypus-TRASHED.plist
+    mv ~/Library/Preferences/org.sveinbjorn.platypus.plist ~/.Trash/org.sveinbjorn.platypus-TRASHED-$RANDOM.plist
 fi
 
 if [ -e $1/../../../Platypus.app ]
 then
     echo "Moving Platypus.app to Trash" > /dev/stderr
-    mv $1/../../../Platypus.app ~/.Trash/Platypus-TRASHED.app
+    mv $1/../../../Platypus.app ~/.Trash/Platypus-TRASHED-$RANDOM.app
 fi
