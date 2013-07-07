540bdbed1761749381cf68b0d1dafb9aa80994bc
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2013-07-07 21:37:19 +0000

    Updated documentation + documentation script now copies Readme to server

--- Documentation/update_docs.sh
+++ Documentation/update_docs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Copies Platypus documentation files to public web server
 
 scp PlatypusDocumentation.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/PlatypusDocumentation.html
+scp Readme.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/Readme.html
 scp -r images root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/
