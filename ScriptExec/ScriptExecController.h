d300357fd45d3dc0aa868c4e0d71c3950da27002
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-03-30 19:00:58 +0000

    (c)2014 --> (c)2015

--- ScriptExec/ScriptExecController.h
+++ ScriptExec/ScriptExecController.h
@@ -1,6 +1,6 @@
 /*
  ScriptExec - binary bundled into Platypus-generated applications
- Copyright (C) 2003-2014 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
+ Copyright (C) 2003-2015 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
  
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
