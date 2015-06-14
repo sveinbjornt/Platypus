7da6f70c540e0580d03f22dcd669e3be2ff6cfa0
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-06-14 18:00:46 +0000

    Upd

--- PlatypusApplication/SuffixList.m
+++ PlatypusApplication/SuffixList.m
@@ -1,21 +1,30 @@
 /*
- Platypus - program for creating Mac OS X application wrappers around scripts
- Copyright (C) 2003-2015 Sveinbjorn Thordarson <sveinbjornt@gmail.com>
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- 
+Copyright (c) 2003-2015, Sveinbjorn Thordarson <sveinbjornt@gmail.com
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the FreeBSD Project.
  */
 
 // SuffixList is a controller class around the Suffix list in the Platypus
@@ -39,7 +48,7 @@
 }
 
 - (NSString *)getSuffixAtIndex:(int)index {
-    return ([[items objectAtIndex:index] objectAtIndex:0]);
+    return ([[items objectAtIndex:index] objectForKey:@"suffix"]);
 }
 
 - (int)numberOfRowsInTableView:(NSTableView *)aTableView {
@@ -50,7 +59,7 @@
     //return ([items objectAtIndex: rowIndex]);
     
     if ([[aTableColumn identifier] caseInsensitiveCompare:@"2"] == NSOrderedSame) {
-        return([[items objectAtIndex:rowIndex] objectAtIndex:0]);
+        return([[items objectAtIndex:rowIndex] objectForKey:@"suffix"]);
     }
     else if ([[aTableColumn identifier] caseInsensitiveCompare:@"1"] == NSOrderedSame) {
         if (rowIndex == 0) {
@@ -59,7 +68,7 @@
             [aTableColumn setDataCell:iconCell];
         }
         
-        return [[items objectAtIndex:rowIndex] objectAtIndex:1];
+        return [[items objectAtIndex:rowIndex] objectForKey:@"icon"];
     }
     return(@"");
 }
@@ -68,7 +77,8 @@
     if ([self hasSuffix:suffix])
         return;
     NSImage *icon = [[NSWorkspace sharedWorkspace] iconForFileType:suffix];
-    [items addObject:[NSArray arrayWithObjects:suffix, icon, nil]];
+    NSDictionary *infoDict = [NSDictionary dictionaryWithObjectsAndKeys:@"suffix", suffix, @"icon", icon, nil];
+    [items addObject:infoDict];
 }
 
 - (void)addSuffixes:(NSArray *)suffixes {
@@ -76,14 +86,14 @@
     
     for (i = 0; i < [suffixes count]; i++) {
         NSImage *icon = [[NSWorkspace sharedWorkspace] iconForFileType:[suffixes objectAtIndex:i]];
-        [items addObject:[NSArray arrayWithObjects:[suffixes objectAtIndex:i], icon, nil]];
+        [items addObject:[NSDictionary dictionaryWithObjectsAndKeys: @"suffix", [suffixes objectAtIndex:i], @"icon", icon, nil]];
     }
 }
 
 - (BOOL)hasSuffix:(NSString *)suffix {
     int i;
     for (i = 0; i < [items count]; i++) {
-        if ([[[items objectAtIndex:i] objectAtIndex:0] isEqualToString:suffix])
+        if ([[[items objectAtIndex:i] objectForKey:@"suffix"] isEqualToString:suffix])
             return YES;
     }
     return NO;
@@ -92,7 +102,7 @@
 - (BOOL)hasAllSuffixes {
     int i;
     for (i = 0; i < [items count]; i++) {
-        if ([[[items objectAtIndex:i] objectAtIndex:0] isEqualToString:@"*"])
+        if ([[[items objectAtIndex:i] objectForKey:@"suffix"] isEqualToString:@"*"])
             return YES;
     }
     return NO;
@@ -116,7 +126,7 @@
     NSMutableArray *suffices = [NSMutableArray arrayWithCapacity:PROGRAM_MAX_LIST_ITEMS];
     
     for (i = 0; i < [items count]; i++) {
-        [suffices addObject:[[items objectAtIndex:i] objectAtIndex:0]];
+        [suffices addObject:[[items objectAtIndex:i] objectForKey:@"suffix"]];
     }
     
     return suffices;
