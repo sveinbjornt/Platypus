78a750a99b7308b3941b0fe354d856cafb634be8
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-06-15 00:45:37 +0000

    Source code is now BSD licensed (enjoy, folks)

--- Shared/NSColor+HexTools.m
+++ Shared/NSColor+HexTools.m
@@ -1,32 +1,36 @@
 /*
+ Copyright (c) 2003-2015, Sveinbjorn Thordarson <sveinbjornt@gmail.com
+ All rights reserved.
  
- NSColor+HexTools - Functions for getting hex string from color and vice versa
+ Redistribution and use in source and binary forms, with or without modification,
+ are permitted provided that the following conditions are met:
  
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+ 1. Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
  
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+ 2. Redistributions in binary form must reproduce the above copyright notice, this
+ list of conditions and the following disclaimer in the documentation and/or other
+ materials provided with the distribution.
  
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ 3. Neither the name of the copyright holder nor the names of its contributors may
+ be used to endorse or promote products derived from this software without specific
+ prior written permission.
  
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "NSColor+HexTools.h"
 
 @implementation NSColor (HexTools)
-/*
- 
- NSColor: Instantiate from Web-like Hex RRGGBB string
- Original Source: <http://cocoa.karelia.com/Foundation_Categories/NSColor__Instantiat.m>
- 
- */
 
 + (NSColor *)colorFromHex:(NSString *)inColorString {
     NSString *charStr       = [inColorString substringFromIndex:1];
