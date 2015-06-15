9490461da4cc6ced045201d00b37c713f1518304
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-06-15 00:49:36 +0000

    Updated docs

--- Documentation/update_docs.sh
+++ Documentation/update_docs.sh
@@ -3,4 +3,5 @@
 
 scp Documentation.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/Documentation.html
 scp Readme.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/Readme.html
+scp License.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/License.html
 scp -r images root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/
