01565243683cd59ceb27cae8169b6e3ee818da9b
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-11-08 16:41:27 +0000

    New high resolution (1024x1024) application icons + icon build script and assets

---
+++ Icons/make_icons.sh
@@ -0,0 +1,28 @@
+mkdir PlatypusAppIcon.iconset
+sips -z 16 16     PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_16x16.png
+sips -z 32 32     PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_16x16@2x.png
+sips -z 32 32     PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_32x32.png
+sips -z 64 64     PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_32x32@2x.png
+sips -z 128 128   PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_128x128.png
+sips -z 256 256   PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_128x128@2x.png
+sips -z 256 256   PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_256x256.png
+sips -z 512 512   PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_256x256@2x.png
+sips -z 512 512   PlatypusAppIcon1024.png --out PlatypusAppIcon.iconset/icon_512x512.png
+cp PlatypusAppIcon1024.png PlatypusAppIcon.iconset/icon_512x512@2x.png
+iconutil -c icns PlatypusAppIcon.iconset
+rm -R PlatypusAppIcon.iconset
+
+
+mkdir PlatypusDefault.iconset
+sips -z 16 16     PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_16x16.png
+sips -z 32 32     PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_16x16@2x.png
+sips -z 32 32     PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_32x32.png
+sips -z 64 64     PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_32x32@2x.png
+sips -z 128 128   PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_128x128.png
+sips -z 256 256   PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_128x128@2x.png
+sips -z 256 256   PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_256x256.png
+sips -z 512 512   PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_256x256@2x.png
+sips -z 512 512   PlatypusDefault1024.png --out PlatypusDefault.iconset/icon_512x512.png
+cp PlatypusDefault1024.png PlatypusDefault.iconset/icon_512x512@2x.png
+iconutil -c icns PlatypusDefault.iconset
+rm -R PlatypusDefault.iconset
