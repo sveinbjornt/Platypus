ab2f9416615a2f20864dcb8e82f971499af7b948
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-11-13 20:56:05 +0000

    New much faster method to calculate folder sizes in bundled files list

--- Shared/NSWorkspace+Additions.m
+++ Shared/NSWorkspace+Additions.m
@@ -29,19 +29,112 @@
  */
 
 #import "NSWorkspace+Additions.h"
+#import <sys/stat.h>
+#import <sys/types.h>
+#import <dirent.h>
 
 @implementation NSWorkspace (Additions)
 
 #pragma mark - File/folder size
 
-- (UInt64)fileOrFolderSize:(NSString *)path {
-    UInt64 size = 0;
-    BOOL isDir;
+//  Copyright (c) 2015 Nikolai Ruhe. All rights reserved.
+//
+// This method calculates the accumulated size of a directory on the volume in bytes.
+//
+// As there's no simple way to get this information from the file system it has to crawl the entire hierarchy,
+// accumulating the overall sum on the way. The resulting value is roughly equivalent with the amount of bytes
+// that would become available on the volume if the directory would be deleted.
+//
+// Caveat: There are a couple of oddities that are not taken into account (like symbolic links, meta data of
+// directories, hard links, ...).
+
+- (BOOL)getAllocatedSize:(unsigned long long *)size ofDirectoryAtURL:(NSURL *)directoryURL error:(NSError * __autoreleasing *)error
+{
+    NSParameterAssert(size != NULL);
+    NSParameterAssert(directoryURL != nil);
     
-    if (path == nil || ![[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir]) {
-        return size;
+    // We'll sum up content size here:
+    unsigned long long accumulatedSize = 0;
+    
+    // prefetching some properties during traversal will speed up things a bit.
+    NSArray *prefetchedProperties = [NSArray arrayWithObjects:
+                                      NSURLIsRegularFileKey,
+                                      NSURLFileAllocatedSizeKey,
+                                      NSURLTotalFileAllocatedSizeKey,
+                                      nil];
+    
+    // The error handler simply signals errors to outside code.
+    __block BOOL errorDidOccur = NO;
+    BOOL (^errorHandler)(NSURL *, NSError *) = ^(NSURL *url, NSError *localError) {
+        if (error != NULL) {
+            *error = localError;
+        }
+        errorDidOccur = YES;
+        return NO;
+    };
+    
+    // We have to enumerate all directory contents, including subdirectories.
+    NSDirectoryEnumerator *enumerator = [[NSFileManager defaultManager] enumeratorAtURL:directoryURL
+                                                             includingPropertiesForKeys:prefetchedProperties
+                                                                                options:(NSDirectoryEnumerationOptions)0
+                                                                           errorHandler:errorHandler];
+    
+    // Start the traversal:
+    for (NSURL *contentItemURL in enumerator) {
+        
+        // Bail out on errors from the errorHandler.
+        if (errorDidOccur)
+            return NO;
+        
+        // Get the type of this item, making sure we only sum up sizes of regular files.
+        NSNumber *isRegularFile;
+        if (! [contentItemURL getResourceValue:&isRegularFile forKey:NSURLIsRegularFileKey error:error])
+            return NO;
+        if (! [isRegularFile boolValue])
+            continue; // Ignore anything except regular files.
+        
+        // To get the file's size we first try the most comprehensive value in terms of what the file may use on disk.
+        // This includes metadata, compression (on file system level) and block size.
+        NSNumber *fileSize;
+        if (! [contentItemURL getResourceValue:&fileSize forKey:NSURLTotalFileAllocatedSizeKey error:error])
+            return NO;
+        
+        // In case the value is unavailable we use the fallback value (excluding meta data and compression)
+        // This value should always be available.
+        if (fileSize == nil) {
+            if (! [contentItemURL getResourceValue:&fileSize forKey:NSURLFileAllocatedSizeKey error:error])
+                return NO;
+            
+            NSAssert(fileSize != nil, @"huh? NSURLFileAllocatedSizeKey should always return a value");
+        }
+        
+        // We're good, add up the value.
+        accumulatedSize += [fileSize unsignedLongLongValue];
     }
     
+    // Bail out on errors from the errorHandler.
+    if (errorDidOccur)
+        return NO;
+    
+    // We finally got it.
+    *size = accumulatedSize;
+    return YES;
+}
+
+- (unsigned long long)nrCalculateFolderSize:(NSString *)folderPath {
+    unsigned long long size = 0;
+    NSURL *url = [NSURL fileURLWithPath:folderPath];
+    [self getAllocatedSize:&size ofDirectoryAtURL:url error:nil];
+    return size;
+}
+
+- (UInt64)fileOrFolderSize:(NSString *)path {
+    BOOL isDir;
+    if (path == nil || ![[NSFileManager defaultManager] fileExistsAtPath:path isDirectory:&isDir]) {
+        return 0;
+    }
+    
+    UInt64 size = 0;
     if (isDir) {
         NSDirectoryEnumerator *dirEnumerator = [[NSFileManager defaultManager] enumeratorAtPath:path];
         while ([dirEnumerator nextObject]) {
@@ -49,8 +142,9 @@
                 size += [[dirEnumerator fileAttributes] fileSize];
             }
         }
+        size = [[NSWorkspace sharedWorkspace] nrCalculateFolderSize:path];
     } else {
-        size = [[FILEMGR attributesOfItemAtPath:path error:nil] fileSize];
+        size = [[[NSFileManager defaultManager] attributesOfItemAtPath:path error:nil] fileSize];
     }
     
     return size;
@@ -167,7 +261,7 @@
         return FALSE;
     }
     
-    [WORKSPACE openFile:path withApplication:app];
+    [[NSWorkspace sharedWorkspace] openFile:path withApplication:app];
     return TRUE;
 }
 
