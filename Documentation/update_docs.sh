b952cd0cf66589c22288bfd72e2c047984ae1e00
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-11-30 00:39:31 +0000

    Version 5.0 released!

--- Documentation/update_docs.sh
+++ Documentation/update_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Copies Platypus documentation files to public web server
 
-scp Documentation.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/Documentation.html
-scp Readme.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/Readme.html
-scp License.html root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/License.html
-scp -r images root@sveinbjorn.org:/www/sveinbjorn/html/files/software/platypus/documentation/
+scp Documentation.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/Documentation.html
+scp Readme.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/Readme.html
+scp License.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/License.html
+scp -r images root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/
