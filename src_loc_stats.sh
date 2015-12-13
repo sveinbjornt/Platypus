eab5d1c5309065994b84d32364c9ab270dba93cc
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2015-12-13 22:21:55 +0000

    Improved LOC count script

--- src_loc_stats.sh
+++ src_loc_stats.sh
@@ -3,19 +3,36 @@
 # Print total lines of code for project
 #
 echo "Lines of code in Platypus codebase"
+
 echo "LOC Total:"
 find . -name \*.\[m\|h\] -exec cat {} \; | wc -l
+
 echo "LOC Total implementation files"
 find . -name \*.\[m\] -exec cat {} \; | wc -l
+
 echo "LOC Total header files"
 find . -name \*.\[h\] -exec cat {} \; | wc -l
+
 echo "LOC Platypus App"
-find Application -name \*.\[m\|h\] -exec cat {} \; | wc -l
+APP_TOTAL=`find Application -name \*.\[m\|h\] -exec cat {} \; | wc -l`
+APP_IMPL=`find Application -name \*.\[m\] -exec cat {} \; | wc -l`
+APP_HEAD=`find Application -name \*.\[h\] -exec cat {} \; | wc -l`
+echo "${APP_TOTAL} (${APP_HEAD}/${APP_IMPL})"
+
 echo "LOC ScriptExec"
-find ScriptExec -name \*.\[m\|h\] -exec cat {} \; | wc -l
+SCRIPTEXEC_TOTAL=`find ScriptExec -name \*.\[m\|h\] -exec cat {} \; | wc -l`
+SCRIPTEXEC_IMPL=`find ScriptExec -name \*.\[m\] -exec cat {} \; | wc -l`
+SCRIPTEXEC_HEAD=`find ScriptExec -name \*.\[h\] -exec cat {} \; | wc -l`
+echo "${SCRIPTEXEC_TOTAL} (${SCRIPTEXEC_HEAD}/${SCRIPTEXEC_IMPL})"
+
 echo "LOC Command Line Tool"
 find CommandLineTool -name \*.\[m\|h\] -exec cat {} \; | wc -l
+
 echo "LOC Shared"
-find Shared -name \*.\[m\|h\] -exec cat {} \; | wc -l
+SHARED_TOTAL=`find Shared -name \*.\[m\|h\] -exec cat {} \; | wc -l`
+SHARED_IMPL=`find Shared -name \*.\[m\] -exec cat {} \; | wc -l`
+SHARED_HEAD=`find Shared -name \*.\[h\] -exec cat {} \; | wc -l`
+echo "${SHARED_TOTAL} (${SHARED_HEAD}/${SHARED_IMPL})"
+
 echo "LOC Shared (Others)"
 find Shared/Others -name \*.\[m\|h\] -exec cat {} \; | wc -l
