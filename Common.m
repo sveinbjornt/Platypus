c29804660f2ff27c93afd07dc9e7e7a8081cd7f3
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2016-01-12 00:22:46 +0000

    Got rid of redundant duplicate code in bundle identifier validation function

--- Common.m
+++ Common.m
@@ -105,9 +105,5 @@ BOOL UTTypeIsValid(NSString *inUTI) {
 BOOL BundleIdentifierIsValid(NSString *bundleIdentifier) {
     // identical to UTType for now, but should really
     // check if the reverse DNS has all three required components
-    NSString *reverseDNSRegEx = @"^[A-Za-z]{2,6}((?!-)\\.[A-Za-z0-9-]{1,63}(?<!-))+$";
-    NSPredicate *test = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", reverseDNSRegEx];
-    return [test evaluateWithObject:bundleIdentifier];
+    return UTTypeIsValid(bundleIdentifier);
 }
-
-
