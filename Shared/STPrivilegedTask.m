1b6f20bc500bc894bb846c9b9ab1f7a2a85fcd54
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2019-03-06 13:45:41 +0000

    Updated to latest version of STPrivilegedTask which doesn't have ARC support

--- Shared/STPrivilegedTask.m
+++ Shared/STPrivilegedTask.m
@@ -1,30 +1,30 @@
 /*
- # STPrivilegedTask - NSTask-like wrapper around AuthorizationExecuteWithPrivileges
- # Copyright (C) 2009-2018 Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
- #
- # BSD License
- # Redistribution and use in source and binary forms, with or without
- # modification, are permitted provided that the following conditions are met:
- #     * Redistributions of source code must retain the above copyright
- #       notice, this list of conditions and the following disclaimer.
- #     * Redistributions in binary form must reproduce the above copyright
- #       notice, this list of conditions and the following disclaimer in the
- #       documentation and/or other materials provided with the distribution.
- #     * Neither the name of the copyright holder nor that of any other
- #       contributors may be used to endorse or promote products
- #       derived from this software without specific prior written permission.
- # 
- # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- # DISCLAIMED. IN NO EVENT SHALL  BE LIABLE FOR ANY
- # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+    STPrivilegedTask - NSTask-like wrapper around AuthorizationExecuteWithPrivileges
+    Copyright (C) 2009-2019 Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
+
+    BSD License
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+        notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+        notice, this list of conditions and the following disclaimer in the
+        documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor that of any other
+        contributors may be used to endorse or promote products
+        derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL  BE LIABLE FOR ANY
+    DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 #import "STPrivilegedTask.h"
 
@@ -110,9 +110,6 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 + (STPrivilegedTask *)launchedPrivilegedTaskWithLaunchPath:(NSString *)path
 {
     STPrivilegedTask *task = [[STPrivilegedTask alloc] initWithLaunchPath:path];
-#if !__has_feature(objc_arc)
-    [task autorelease];
-#endif
     [task launch];
     [task waitUntilExit];
     return task;
@@ -121,10 +118,6 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 + (STPrivilegedTask *)launchedPrivilegedTaskWithLaunchPath:(NSString *)path arguments:(NSArray *)args
 {
     STPrivilegedTask *task = [[STPrivilegedTask alloc] initWithLaunchPath:path arguments:args];
-#if !__has_feature(objc_arc)
-    [task autorelease];
-#endif
-    
     [task launch];
     [task waitUntilExit];
     return task;
@@ -133,10 +126,6 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 + (STPrivilegedTask *)launchedPrivilegedTaskWithLaunchPath:(NSString *)path arguments:(NSArray *)args currentDirectory:(NSString *)cwd
 {
     STPrivilegedTask *task = [[STPrivilegedTask alloc] initWithLaunchPath:path arguments:args currentDirectory:cwd];
-#if !__has_feature(objc_arc)
-    [task autorelease];
-#endif
-    
     [task launch];
     [task waitUntilExit];
     return task;
@@ -145,10 +134,6 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 + (STPrivilegedTask *)launchedPrivilegedTaskWithLaunchPath:(NSString *)path arguments:(NSArray *)args currentDirectory:(NSString *)cwd authorization:(AuthorizationRef)authorization
 {
     STPrivilegedTask *task = [[STPrivilegedTask alloc] initWithLaunchPath:path arguments:args currentDirectory:cwd];
-#if !__has_feature(objc_arc)
-    [task autorelease];
-#endif
-    
     [task launchWithAuthorization:authorization];
     [task waitUntilExit];
     return task;
@@ -215,7 +200,6 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
     
     // Assuming the authorization is valid for the task.
     // Let's prepare to launch it
-    
     NSArray *arguments = self.arguments;
     NSUInteger numberOfArguments = [arguments count];
     char *args[numberOfArguments + 1];
@@ -268,7 +252,7 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 
 - (void)terminate
 {
-    // This doesn't work without a PID, and we can't get one.  Stupid Security API
+    // This doesn't work without a PID, and we can't get one. Stupid Security API.
 //    int ret = kill(pid, SIGKILL);
 //     
 //    if (ret != 0) {
@@ -316,8 +300,7 @@ static OSStatus (*_AuthExecuteWithPrivsFn)(AuthorizationRef authorization, const
 + (BOOL)authorizationFunctionAvailable
 {
     if (!_AuthExecuteWithPrivsFn) {
-        // This version of OS X has finally removed
-        // this function. Return with an error.
+        // This version of OS X has finally removed this function. Return with an error.
         return NO;
     }
     return YES;
