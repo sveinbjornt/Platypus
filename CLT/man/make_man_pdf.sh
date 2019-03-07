671dc952b829d8e96f032f243f8bff56893248c0
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2019-03-07 18:19:49 +0000

    Removed unused man2pdf script, updated .gitignore

--- CLT/man/make_man_pdf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-groff -mandoc platypus.1 | pstopdf -i -o platypus.man.pdf
--- CLT/man/update_online_manpage.sh
+++ CLT/man/update_online_manpage.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#scp platypus.man.pdf root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/platypus.man.pdf
 scp platypus.man.html root@sveinbjorn.org:/www/sveinbjorn/html/files/manpages/platypus.man.html
