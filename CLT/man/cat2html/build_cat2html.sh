ff8cf492539a8a747d1877ddd44563be1b06529c
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-06-08 18:01:55 +0000

    Fixes to cat2html lex, cat2html binary now both x86_64 and arm64

--- CLT/man/cat2html/build_cat2html.sh
+++ CLT/man/cat2html/build_cat2html.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 lex cat2html.l
-gcc -O2 lex.cat2html.c -o cat2html
+gcc -O2 -arch 'x86_64' -arch 'arm64' lex.cat2html.c -o cat2html
 rm lex.cat2html.c
 strip -x cat2html
 chmod +x cat2html
